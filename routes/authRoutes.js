25de96d0d3ea43d25d0a70462aeebe639325fc67
Ramona <rare2400@student.miun.com>
2025-10-21 15:44:15 +0200

    gör om ordningen av CRUD-operationerna

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,51 +13,6 @@ const authenticateToken = require("../middleware/authenticateToken");
 const User = require("../models/admin");
 
 
-//router to get all users
-router.get("/users", authenticateToken, async (req, res) => {
-    try {
-        const result = await User.find({}, { password: 0 });
-        res.status(200).json(result);
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        res.status(500).json({ error: "Server error", details: error.message });
-    }
-});
-
-//router to get user by id
-router.get("/users/:id", authenticateToken, async (req, res) => {
-    try {
-        const result = await User.findById({ _id: req.params.id }, { password: 0 });
-
-        if (!result) {
-            return res.status(404).json({ error: "User not found" });
-        }
-
-        res.status(200).json(result);
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        res.status(500).json({ error: "Server error" });
-    }
-});
-
-//router to update user by id
-router.put("/users/:id", authenticateToken, async (req, res) => {
-    try {
-        let result = await User.findOneAndUpdate({ _id: req.params.id }, req.body);
-
-        result = await User.findOne({ _id: req.params.id }, { password: 0 });
-
-        if (!result) {
-            return res.status(404).json({ error: "User not found" });
-        }
-
-        res.status(200).json(result);
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        res.status(500).json({ error: "Server error" });
-    }
-});
-
 //register user
 router.post("/register", async (req, res) => {
     try {
@@ -118,12 +73,57 @@ router.post("/login", async (req, res) => {
     }
 });
 
+//router to get all users
+router.get("/users", authenticateToken, async (req, res) => {
+    try {
+        const result = await User.find({}, { password: 0 });
+        res.status(200).json(result);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Server error", details: error.message });
+    }
+});
+
+//router to get user by id
+router.get("/users/:id", authenticateToken, async (req, res) => {
+    try {
+        const result = await User.findById({ _id: req.params.id }, { password: 0 });
+
+        if (!result) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.status(200).json(result);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+//router to update user by id
+router.put("/users/:id", authenticateToken, async (req, res) => {
+    try {
+        let result = await User.findOneAndUpdate({ _id: req.params.id }, req.body);
+
+        result = await User.findOne({ _id: req.params.id }, { password: 0 });
+
+        if (!result) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.status(200).json(result);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
 //delete user by id
 router.delete("/users/:id", authenticateToken, async (req, res) => {
     try {
         const result = await User.findByIdAndDelete(req.params.id);
 
-        if(!result) {
+        if (!result) {
             return res.status(404).json({ error: "User not found" });
         }
 
