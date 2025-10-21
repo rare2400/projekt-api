f7cc941bcfc102429f810ae5d4f7b811752b4c15
Ramona <rare2400@student.miun.com>
2025-10-21 15:02:56 +0200

    tar bort dotenv då den inte används, och importeras redan i server.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -6,7 +6,6 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middleware/authenticateToken");
-require("dotenv").config();
 
 const Message = require("../models/messages");
 
